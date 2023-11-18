bfe6aa76f9aa2ac3c55b6184f3ebacc5d408c730
hectorgarcia07 <csharp12321@gmail.com>
2023-11-17 21:35:52 -0600

    Made a basic validation for the date

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hellow world")
+//will be used to access the day, month, and year
+const year = document.getElementById('year-el')
+const month = document.getElementById('month-el')
+const day = document.getElementById('day-el')
+
+//function that will be used to determine if date is valid
+function isDateValid() {
+    //if all the year, month, and day is not empty
+    //check if it's valid
+    if(year.value && month.value && day.value){
+        //if the year is valid display the persons age
+        //otherwise date is incorrect
+        const date = `${month.value}/${day.value}/${year.value}`
+
+        console.log("Date is ", date)
+        
+        if(!isNaN(new Date(date))){
+            console.log("Date is valid!")
+        }
+        else{
+            console.log("Date is not valid!")
+        }
+    }
+
+}
+
+year.addEventListener("input", isDateValid)
+month.addEventListener("input", isDateValid)
+day.addEventListener("input", isDateValid)
