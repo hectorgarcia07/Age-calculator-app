dffbcd87bddb5e2b770ca5b49818fa90e33ff6cf
hectorgarcia07 <csharp12321@gmail.com>
2023-11-18 22:00:49 -0600

    added some validation regex and js

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const month = document.getElementById('month-el')
 const day = document.getElementById('day-el')
 
 //function that will be used to determine if date is valid
-function isDateValid() {
+function isDateValid(event) {
+    console.log(event.target.id)
+
+
+    switch(event.target.id){
+        case 'day-el':
+            if(event.target.value.match(new RegExp(/([1-9]|[12][0-9]|3[01])/g))){
+                console.log("VALID")
+            }
+            break
+    }
     //if all the year, month, and day is not empty
     //check if it's valid
     if(year.value && month.value && day.value){
@@ -24,6 +34,6 @@ function isDateValid() {
 
 }
 
-year.addEventListener("input", isDateValid)
-month.addEventListener("input", isDateValid)
-day.addEventListener("input", isDateValid)
+year.addEventListener("input", (e) => isDateValid(e))
+month.addEventListener("input", (e) => isDateValid(e))
+day.addEventListener("input", (e) => isDateValid(e))
